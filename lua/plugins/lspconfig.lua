c57306f075a46f9031b45d0dfba627178d08249e
Chris Covert <chris.covert@blackboard.com>
2021-08-19 12:04:37 -0400

    remove coq schedule hack

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,15 +1,10 @@
 local M = {}
 
-M.config = function()
-  local lsp = require'lsp'
-  vim.schedule(lsp.setup_lsp)
-end
-
 M.plugin = {
   'neovim/nvim-lspconfig',
   opt = false,
   requires = {'ms-jpq/coq_nvim'},
-  config = [[require'plugins.lspconfig'.config()]],
+  config = [[require'lsp'.setup_lsp()]],
 }
 
 return M
