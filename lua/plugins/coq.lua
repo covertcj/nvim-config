4486f698b1f27181f6a9e6cb3884620c9d941767
Christopher J Covert <covertops5@gmail.com>
2021-08-29 15:49:55 -0400

    defer loading COQ to avoid COQnow not existing

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 M.config = function ()
-  vim.cmd [[COQnow --shut-up]]
+  -- automatically initalize COQ after 2.5 seconds
+  -- sort of a workaround for an issue where COQnow seems to be undefined
+  -- *sometimes* when executed immediately
+  vim.defer_fn(function()
+    vim.cmd [[COQnow --shut-up]]
+  end, 2000)
 end
 
 M.plugin = {
