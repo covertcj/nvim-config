eadb5ccd967f06b71fd6a3d75ded0adcdb0d5698
Chris Covert <chris.covert@blackboard.com>
2022-01-30 01:12:52 -0500

    feat: lsp/file hotkeys

--- lua/cjc/whichkey.lua
+++ lua/cjc/whichkey.lua
@@ -1,11 +1,11 @@
-local status_ok, which_key = pcall(require, "which-key")
+local status_ok, which_key = pcall(require, 'which-key')
 if not status_ok then
   return
 end
 
 local n_opts = {
-  mode = "n",
-  prefix = "<leader>",
+  mode = 'n',
+  prefix = '<leader>',
   buffer = nil,
   silent = true,
   noremap = true,
@@ -14,14 +14,36 @@ local n_opts = {
 
 local n_mappings = {
   f = {
-    name = "file",
-    f = { "<CMD>Telescope find_files<CR>", "find file" },
-    g = { "<CMD>Telescope live_grep<CR>", "grep" },
+    name = 'file',
+    f = { '<CMD>Telescope find_files<CR>', 'find file' },
+    g = { '<CMD>Telescope live_grep<CR>', 'grep' },
+    s = { '<CMD>w<CR>', 'save' },
   },
 
   o = {
-    name = "open",
-    e = { "<CMD>Lexplore 30<CR>", "explorer" },
+    name = 'open',
+    e = { '<CMD>Lexplore 30<CR>', 'explorer' },
+  },
+
+  l = {
+    name = 'lsp',
+    a = { '<CMD>lua vim.lsp.buf.code_action()<CR>', 'code action' },
+    j = { '<CMD>lua vim.lsp.diagnostic.goto_next()<CR>', 'next diagnostic' },
+    k = { '<CMD>lua vim.lsp.diagnostic.goto_prev()<CR>', 'prev diagnostic' },
+    r = { '<CMD>lua vim.lsp.buf.rename()<CR>', 'rename' },
+    d = { '<CMD>Telescope lsp_document_diagnostics<CR>', 'diagnostics', },
+    D = { '<CMD>Telescope lsp_workspace_diagnostics<CR>', 'workspace diagnostics', },
+    s = { '<CMD>Telescope lsp_document_symbols<CR>', 'symbols', },
+    S = { '<CMD>Telescope lsp_workspace_symbols<CR>', 'workspace symbols', },
+    i = { '<CMD>LspInfo<CR>', 'lsp info', },
+    i = { '<CMD>LspInstallInfo<CR>', 'lsp install info', },
+  },
+
+  P = {
+    name = 'packer',
+    s = { '<CMD>PackerSync<CR>', 'sync' },
+    S = { '<CMD>PackerStatus<CR>', 'status' },
+    u = { '<CMD>PackerUpdate<CR>', 'update' },
   },
 }
 
